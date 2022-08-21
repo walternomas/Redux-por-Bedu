3ff3e434e190c284fa3efacd2079e838cb53f7d1
walternomas <w.barriosvazquez@gmail.com>
2022-08-20 21:38:36 -0300

    axios

--- blog_platzi/src/components/App.js
+++ blog_platzi/src/components/App.js
@@ -1,34 +1,46 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      usuarios: [],
+    };
+  }
+
+  async componentDidMount() {
+    console.log("Se montó");
+    const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+    console.log(respuesta);
+
+    this.setState({
       usuarios: [
         {
           nombre: "Walter",
           correo: "w.barriosvazquez@gmail.com",
           enlace: "wbit.ar",
-        },{
+        },
+        {
           nombre: "Platzi",
           correo: "team@platzi.com",
           enlace: "platzi.com",
-        }
-      ]
-    };
+        },
+      ],
+    });
   }
-  ponerFilas = () => (
+
+  ponerFilas = () =>
     this.state.usuarios.map((usuario) => (
-        <tr>
-          <td>{usuario.nombre}</td>
-          <td>{usuario.correo}</td>
-          <td>{usuario.enlace}</td>
-        </tr>
-      )
-    )
-  );
+      <tr>
+        <td>{usuario.nombre}</td>
+        <td>{usuario.correo}</td>
+        <td>{usuario.enlace}</td>
+      </tr>
+    ));
 
   render() {
+    console.log(this.state.usuarios);
     return (
       <div className="margen">
         <table className="tabla">
